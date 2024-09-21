ae8bf698c779706f449370c19696c6c7384c3cef
unlimmitted <kosnmasss@gmail.com>
2024-09-21 14:41:24 +0300

    added Docker

--- Script.js
+++ Script.js
@@ -1,33 +1,33 @@
-var z = 1;
-var a = 1000;
-var b = 10000;
+let z = 1;
+let a = 1000;
+let b = 10000;
 
 function plus() {
-    a = 1000;
-    b = 10000;
-    z = z + 1;
-    a = a * z;
-    b = b * z;
+	a = 1000;
+	b = 10000;
+	z = z + 1;
+	a = a * z;
+	b = b * z;
 
-    document.getElementById("demo").innerHTML = a + " ₽ / " + z + " Ч";
-    document.getElementById("demo_2").innerHTML = b + " ₽ / " + z + " Ч";
-    document.getElementById("demo_1").innerHTML = z;
+	document.getElementById("demo").innerHTML = a + " ₽ / " + z + " Ч";
+	document.getElementById("demo_2").innerHTML = b + " ₽ / " + z + " Ч";
+	document.getElementById("demo_1").innerHTML = z;
 }
 
 function minus() {
-    a = 1000;
-    b = 10000;
-    z = z - 1;
-    a = a * z;
-    b = b * z;
+	a = 1000;
+	b = 10000;
+	z = z - 1;
+	a = a * z;
+	b = b * z;
 
-    if (z == 0) {
-        z = 1;
-        a = 1000;
-        b = 10000;
-    }
+	if (z == 0) {
+		z = 1;
+		a = 1000;
+		b = 10000;
+	}
 
-    document.getElementById("demo").innerHTML = a + " ₽ / " + z + " Ч";
-    document.getElementById("demo_2").innerHTML = b + " ₽ / " + z + " Ч";
-    document.getElementById("demo_1").innerHTML = z;
+	document.getElementById("demo").innerHTML = a + " ₽ / " + z + " Ч";
+	document.getElementById("demo_2").innerHTML = b + " ₽ / " + z + " Ч";
+	document.getElementById("demo_1").innerHTML = z;
 }
