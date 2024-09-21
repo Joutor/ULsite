8bd781b63692a717dbbb0c9b2acedc94f14e380e
petr <petyalom@yandex.ru>
2024-09-21 14:31:11 +0300

    fixes

--- Script.js
+++ Script.js
@@ -1,17 +1,16 @@
 var z = 1;
 var a = 1000;
 var b = 10000;
+
 function plus() {
     a = 1000;
     b = 10000;
     z = z + 1;
     a = a * z;
-    b = b *z;
+    b = b * z;
 
-    document.getElementById("demo").innerHTML =
-    a + " ₽ / " + z + " Ч" ;
-    document.getElementById("demo_2").innerHTML =
-    b + " ₽ / " + z + " Ч" ; 
+    document.getElementById("demo").innerHTML = a + " ₽ / " + z + " Ч";
+    document.getElementById("demo_2").innerHTML = b + " ₽ / " + z + " Ч";
     document.getElementById("demo_1").innerHTML = z;
 }
 
@@ -20,18 +19,15 @@ function minus() {
     b = 10000;
     z = z - 1;
     a = a * z;
-    b = b *z;
+    b = b * z;
 
-    if (z==0) {
+    if (z == 0) {
         z = 1;
         a = 1000;
         b = 10000;
-    } 
+    }
 
-    document.getElementById("demo").innerHTML =
-    a + " ₽ / " + z + " Ч" ; 
-    document.getElementById("demo_2").innerHTML =
-    b + " ₽ / " + z + " Ч" ; 
- 
+    document.getElementById("demo").innerHTML = a + " ₽ / " + z + " Ч";
+    document.getElementById("demo_2").innerHTML = b + " ₽ / " + z + " Ч";
     document.getElementById("demo_1").innerHTML = z;
 }
